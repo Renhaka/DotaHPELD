20c19085a8070e68120d2a2e5b92ec3bcd6bb6df
Zachary F <84silver@gmail.com>
2014-06-09 22:46:17 -0500

    Main Upload
    Main Upload

---
+++ DotaHPLED/Form1.Designer.cs
@@ -0,0 +1,271 @@
+﻿namespace DotaHPLED
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.findProcess = new System.Windows.Forms.Button();
+            this.labelHooked = new System.Windows.Forms.Label();
+            this.labelHookedVariable = new System.Windows.Forms.Label();
+            this.labelPID = new System.Windows.Forms.Label();
+            this.labelPIDVariable = new System.Windows.Forms.Label();
+            this.hookHealth = new System.Windows.Forms.Button();
+            this.labelCurrent = new System.Windows.Forms.Label();
+            this.labelMax = new System.Windows.Forms.Label();
+            this.labelDead = new System.Windows.Forms.Label();
+            this.labelRespawn = new System.Windows.Forms.Label();
+            this.labelCHVar = new System.Windows.Forms.Label();
+            this.labelMHVar = new System.Windows.Forms.Label();
+            this.labelDeathVar = new System.Windows.Forms.Label();
+            this.labelRTVar = new System.Windows.Forms.Label();
+            this.buttonBegin = new System.Windows.Forms.Button();
+            this.labelComPort = new System.Windows.Forms.Label();
+            this.labelArdStatus = new System.Windows.Forms.Label();
+            this.labelComRead = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // findProcess
+            // 
+            this.findProcess.Location = new System.Drawing.Point(12, 12);
+            this.findProcess.Name = "findProcess";
+            this.findProcess.Size = new System.Drawing.Size(114, 23);
+            this.findProcess.TabIndex = 0;
+            this.findProcess.Text = "Find Dota 2 Process";
+            this.findProcess.UseVisualStyleBackColor = true;
+            this.findProcess.Click += new System.EventHandler(this.findProcess_Click);
+            // 
+            // labelHooked
+            // 
+            this.labelHooked.AutoSize = true;
+            this.labelHooked.Location = new System.Drawing.Point(165, 17);
+            this.labelHooked.Name = "labelHooked";
+            this.labelHooked.Size = new System.Drawing.Size(48, 13);
+            this.labelHooked.TabIndex = 1;
+            this.labelHooked.Text = "Hooked:";
+            // 
+            // labelHookedVariable
+            // 
+            this.labelHookedVariable.AutoSize = true;
+            this.labelHookedVariable.Location = new System.Drawing.Point(219, 17);
+            this.labelHookedVariable.Name = "labelHookedVariable";
+            this.labelHookedVariable.Size = new System.Drawing.Size(33, 13);
+            this.labelHookedVariable.TabIndex = 2;
+            this.labelHookedVariable.Text = "Nope";
+            // 
+            // labelPID
+            // 
+            this.labelPID.AutoSize = true;
+            this.labelPID.Location = new System.Drawing.Point(185, 42);
+            this.labelPID.Name = "labelPID";
+            this.labelPID.Size = new System.Drawing.Size(28, 13);
+            this.labelPID.TabIndex = 3;
+            this.labelPID.Text = "PID:";
+            // 
+            // labelPIDVariable
+            // 
+            this.labelPIDVariable.AutoSize = true;
+            this.labelPIDVariable.Location = new System.Drawing.Point(219, 42);
+            this.labelPIDVariable.Name = "labelPIDVariable";
+            this.labelPIDVariable.Size = new System.Drawing.Size(33, 13);
+            this.labelPIDVariable.TabIndex = 4;
+            this.labelPIDVariable.Text = "None";
+            // 
+            // hookHealth
+            // 
+            this.hookHealth.Location = new System.Drawing.Point(12, 42);
+            this.hookHealth.Name = "hookHealth";
+            this.hookHealth.Size = new System.Drawing.Size(114, 23);
+            this.hookHealth.TabIndex = 5;
+            this.hookHealth.Text = "Sniff Info";
+            this.hookHealth.UseVisualStyleBackColor = true;
+            this.hookHealth.Click += new System.EventHandler(this.hookHealth_Click);
+            // 
+            // labelCurrent
+            // 
+            this.labelCurrent.AutoSize = true;
+            this.labelCurrent.Location = new System.Drawing.Point(57, 87);
+            this.labelCurrent.Name = "labelCurrent";
+            this.labelCurrent.Size = new System.Drawing.Size(78, 13);
+            this.labelCurrent.TabIndex = 6;
+            this.labelCurrent.Text = "Current Health:";
+            // 
+            // labelMax
+            // 
+            this.labelMax.AutoSize = true;
+            this.labelMax.Location = new System.Drawing.Point(71, 113);
+            this.labelMax.Name = "labelMax";
+            this.labelMax.Size = new System.Drawing.Size(64, 13);
+            this.labelMax.TabIndex = 7;
+            this.labelMax.Text = "Max Health:";
+            // 
+            // labelDead
+            // 
+            this.labelDead.AutoSize = true;
+            this.labelDead.Location = new System.Drawing.Point(99, 138);
+            this.labelDead.Name = "labelDead";
+            this.labelDead.Size = new System.Drawing.Size(36, 13);
+            this.labelDead.TabIndex = 8;
+            this.labelDead.Text = "Dead:";
+            // 
+            // labelRespawn
+            // 
+            this.labelRespawn.AutoSize = true;
+            this.labelRespawn.Location = new System.Drawing.Point(54, 164);
+            this.labelRespawn.Name = "labelRespawn";
+            this.labelRespawn.Size = new System.Drawing.Size(81, 13);
+            this.labelRespawn.TabIndex = 9;
+            this.labelRespawn.Text = "Respawn Time:";
+            // 
+            // labelCHVar
+            // 
+            this.labelCHVar.AutoSize = true;
+            this.labelCHVar.Location = new System.Drawing.Point(142, 87);
+            this.labelCHVar.Name = "labelCHVar";
+            this.labelCHVar.Size = new System.Drawing.Size(60, 13);
+            this.labelCHVar.TabIndex = 10;
+            this.labelCHVar.Text = "Not Sniffed";
+            // 
+            // labelMHVar
+            // 
+            this.labelMHVar.AutoSize = true;
+            this.labelMHVar.Location = new System.Drawing.Point(142, 113);
+            this.labelMHVar.Name = "labelMHVar";
+            this.labelMHVar.Size = new System.Drawing.Size(60, 13);
+            this.labelMHVar.TabIndex = 11;
+            this.labelMHVar.Text = "Not Sniffed";
+            // 
+            // labelDeathVar
+            // 
+            this.labelDeathVar.AutoSize = true;
+            this.labelDeathVar.Location = new System.Drawing.Point(142, 138);
+            this.labelDeathVar.Name = "labelDeathVar";
+            this.labelDeathVar.Size = new System.Drawing.Size(60, 13);
+            this.labelDeathVar.TabIndex = 12;
+            this.labelDeathVar.Text = "Not Sniffed";
+            // 
+            // labelRTVar
+            // 
+            this.labelRTVar.AutoSize = true;
+            this.labelRTVar.Location = new System.Drawing.Point(142, 164);
+            this.labelRTVar.Name = "labelRTVar";
+            this.labelRTVar.Size = new System.Drawing.Size(60, 13);
+            this.labelRTVar.TabIndex = 13;
+            this.labelRTVar.Text = "Not Sniffed";
+            // 
+            // buttonBegin
+            // 
+            this.buttonBegin.Location = new System.Drawing.Point(12, 196);
+            this.buttonBegin.Name = "buttonBegin";
+            this.buttonBegin.Size = new System.Drawing.Size(96, 23);
+            this.buttonBegin.TabIndex = 15;
+            this.buttonBegin.Text = "Begin Evaluation";
+            this.buttonBegin.UseVisualStyleBackColor = true;
+            this.buttonBegin.Click += new System.EventHandler(this.buttonBegin_Click);
+            // 
+            // labelComPort
+            // 
+            this.labelComPort.AutoSize = true;
+            this.labelComPort.Location = new System.Drawing.Point(114, 201);
+            this.labelComPort.Name = "labelComPort";
+            this.labelComPort.Size = new System.Drawing.Size(73, 13);
+            this.labelComPort.TabIndex = 16;
+            this.labelComPort.Text = "Arduino COM:";
+            // 
+            // labelArdStatus
+            // 
+            this.labelArdStatus.AutoSize = true;
+            this.labelArdStatus.ForeColor = System.Drawing.Color.Red;
+            this.labelArdStatus.Location = new System.Drawing.Point(193, 201);
+            this.labelArdStatus.Name = "labelArdStatus";
+            this.labelArdStatus.Size = new System.Drawing.Size(37, 13);
+            this.labelArdStatus.TabIndex = 17;
+            this.labelArdStatus.Text = "Offline";
+            // 
+            // labelComRead
+            // 
+            this.labelComRead.AutoSize = true;
+            this.labelComRead.Location = new System.Drawing.Point(9, 138);
+            this.labelComRead.Name = "labelComRead";
+            this.labelComRead.Size = new System.Drawing.Size(60, 13);
+            this.labelComRead.TabIndex = 18;
+            this.labelComRead.Text = "NothingYet";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(264, 230);
+            this.Controls.Add(this.labelComRead);
+            this.Controls.Add(this.labelArdStatus);
+            this.Controls.Add(this.labelComPort);
+            this.Controls.Add(this.buttonBegin);
+            this.Controls.Add(this.labelRTVar);
+            this.Controls.Add(this.labelDeathVar);
+            this.Controls.Add(this.labelMHVar);
+            this.Controls.Add(this.labelCHVar);
+            this.Controls.Add(this.labelRespawn);
+            this.Controls.Add(this.labelDead);
+            this.Controls.Add(this.labelMax);
+            this.Controls.Add(this.labelCurrent);
+            this.Controls.Add(this.hookHealth);
+            this.Controls.Add(this.labelPIDVariable);
+            this.Controls.Add(this.labelPID);
+            this.Controls.Add(this.labelHookedVariable);
+            this.Controls.Add(this.labelHooked);
+            this.Controls.Add(this.findProcess);
+            this.Name = "Form1";
+            this.Text = "Dota 2 HP LED Controller";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button findProcess;
+        private System.Windows.Forms.Label labelHooked;
+        private System.Windows.Forms.Label labelHookedVariable;
+        private System.Windows.Forms.Label labelPID;
+        private System.Windows.Forms.Label labelPIDVariable;
+        private System.Windows.Forms.Button hookHealth;
+        private System.Windows.Forms.Label labelCurrent;
+        private System.Windows.Forms.Label labelMax;
+        private System.Windows.Forms.Label labelDead;
+        private System.Windows.Forms.Label labelRespawn;
+        private System.Windows.Forms.Label labelCHVar;
+        private System.Windows.Forms.Label labelMHVar;
+        private System.Windows.Forms.Label labelDeathVar;
+        private System.Windows.Forms.Label labelRTVar;
+        private System.Windows.Forms.Button buttonBegin;
+        private System.Windows.Forms.Label labelComPort;
+        private System.Windows.Forms.Label labelArdStatus;
+        private System.Windows.Forms.Label labelComRead;
+    }
+}
+
---
+++ DotaHPLED/Form1.cs
@@ -0,0 +1,424 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DotaHPLED
+{
+    public partial class Form1 : Form
+    {
+        #region World Vars
+        Byte[] Queend = new Byte[] { 0x51, 0x75, 0x65, 0x00, 0x00, 0x80, 0xBF, 0x00, 0x65, 0x6E, 0x64 };
+        int CurrentHealthOffset = 47;
+        int MaxHealthOffset = 51;
+        int DeadOffset = -22;
+        int TimerOffset = 12799;
+        Process Dota2;
+        IntPtr CHealthAddress;
+        IntPtr MHealthAddress;
+        IntPtr DeadAddress;
+        IntPtr TimerAddress;
+        const int PROCESS_WM_READ = 0x0010;
+        private volatile int _current = 0;
+        private volatile int _max = 1;
+        private volatile int _death = 0;
+        private volatile int _time = 0;
+        SerialPort ard = new SerialPort("COM3", 9600);
+        #endregion
+
+        #region Variable Accessors
+        public int CurrentHealth
+        {
+            get
+            {
+                return _current;
+            }
+            set
+            {
+                _current = value;
+            }
+        }
+        public int MaximumHealth
+        {
+            get
+            {
+                return _max;
+            }
+            set
+            {
+                _max = value;
+            }
+        }
+        public int DeathStatus
+        {
+            get
+            {
+                return _death;
+            }
+            set
+            {
+                _death = value;
+            }
+        }
+        public int RespawnTimer
+        {
+            get
+            {
+                return _time;
+            }
+            set
+            {
+                _time = value;
+            }
+        }
+        #endregion
+
+        #region Form Operations
+        public Form1()
+        {
+            InitializeComponent();
+        }     
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            bob.StopWatching();
+            voyeurThread.Join();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Height = 230;
+            //lights = new Button[] { light1, light2, light3, light4, light5, light6, light7, light8, light9, light10 };
+            //offAll();
+        }
+        #endregion
+
+        #region Button Operations
+        private void findProcess_Click(object sender, EventArgs e)
+        {
+            Process[] tempList = Process.GetProcessesByName("dota");
+            if (tempList.Length > 1)
+            {
+                MessageBox.Show("Multiple instances of Dota 2 found! Narrow these down to one!");
+            }
+            else
+            {
+                Dota2 = tempList[0];
+                labelHookedVariable.Text = "Yes";
+                labelPIDVariable.Text = Dota2.Id.ToString();
+            }
+        }
+        private void hookHealth_Click(object sender, EventArgs e)
+        {
+            this.Height = 270;
+            IntPtr QueendAddress = addressSearch(Dota2, Queend);
+            if (QueendAddress.Equals(IntPtr.Zero))
+            {
+                this.Height = 230;
+                MessageBox.Show("Failure to find");
+            }
+            else
+            {
+                CHealthAddress = new IntPtr(QueendAddress.ToInt32() + CurrentHealthOffset);
+                MHealthAddress = new IntPtr(QueendAddress.ToInt32() + MaxHealthOffset);
+                DeadAddress = new IntPtr(QueendAddress.ToInt32() + DeadOffset);
+                TimerAddress = new IntPtr(QueendAddress.ToInt32() + TimerOffset);
+                try
+                {
+                    ard.Open();
+                    labelArdStatus.Text = "Online";
+                    labelArdStatus.ForeColor = Color.Green;
+                }
+                catch (Exception l)
+                {
+                    MessageBox.Show("Unable to open Arduino port, please try again.\n" + l.ToString());
+                }
+            }
+        }
+        #endregion
+
+        #region Find Address
+        public IntPtr addressSearch(Process dota, byte[] toSearch)
+        {
+            IntPtr p = OpenProcess(0x10, true, dota.Id); //0x10-read
+            this.Height = 270;
+            uint PTR = 0x03000000; //begin of memory
+            byte[] buff = new byte[toSearch.Length];
+            int bytesRead;
+
+            while (PTR < 0x04000000)   //end of memory // u can specify to read less if u know he does not fill it all
+            {
+                ReadProcessMemory(p, new IntPtr(PTR), buff, buff.Length, out bytesRead);
+                int testCase = SpecialByteCompare(buff, toSearch);
+                switch (testCase)
+                {
+                    case 2:
+                        return new IntPtr(PTR);
+                    case 1:
+                        PTR += (uint)toSearch.Length;
+                        break;
+                    case 0:
+                        PTR += 0x1;
+                        break;
+                    case -1:
+                        throw new ImpossibleExceptionException();
+                }
+            }
+            return IntPtr.Zero;
+        }
+
+        private static int SpecialByteCompare(byte[] comparingTo, byte[] comparingWith)  //read memory, first byte array, second byte array, number of missing byte's
+        {
+            if (comparingTo.Length != (comparingWith.Length))
+            {
+                return -1;
+            }
+            if (!comparingTo.Contains<byte>(comparingWith[0]))
+            {
+                return 1;
+            }
+            for (int i = 0; i < comparingWith.Length; i++)
+            {
+                if (comparingTo[i] != comparingWith[i])
+                    return 0;
+            }
+            return 2;
+        }
+
+        #region Imported
+        [DllImport("kernel32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool CloseHandle(IntPtr hObject);
+
+        [DllImport("kernel32.dll")]
+        public static extern IntPtr OpenProcess(int dwDesiredAccess, bool bInheritHandle, int dwProcessId);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static extern bool ReadProcessMemory(
+        IntPtr hProcess,
+        IntPtr lpBaseAddress,
+        [Out] byte[] lpBuffer,
+        int dwSize,
+        out int lpNumberOfBytesRead
+        );
+        #endregion
+        #endregion
+
+        #region Thread
+        Watcher bob;
+        Thread voyeurThread;
+        //Goulding ellie;
+        //Thread lightsThread;
+        bool running = false;
+        
+        delegate void SetTextCallback(int Chealth, int Mhealth, int Dead, int RTime);
+        public void updateNumbers(int Chealth, int Mhealth, int Dead, int RTime)
+        {
+            if (labelCHVar.InvokeRequired || labelDeathVar.InvokeRequired || labelMHVar.InvokeRequired || labelRTVar.InvokeRequired)
+            {
+                SetTextCallback d = new SetTextCallback(updateNumbers);
+                this.Invoke(d, new object[] { Chealth, Mhealth, Dead, RTime });
+            }
+            else
+            {
+                _current = Chealth;
+                _max = Mhealth;
+                _death = (Dead > 70000) ? 0 : 1;
+                _time = (RTime >= 0) ? RTime : 0;
+                ard.Write(_current.ToString()+";"+_max.ToString()+";"+_death.ToString()+";"+_time.ToString());
+                labelCHVar.Text = _current.ToString();
+                labelMHVar.Text = _max.ToString();
+                labelDeathVar.Text = _death.ToString();
+                labelRTVar.Text = _time.ToString();
+                labelComRead.Text = ard.ReadLine();
+            }
+        }
+        private void buttonBegin_Click(object sender, EventArgs e)
+        {
+            if (!running)
+            {
+                bob = new Watcher(Dota2, CHealthAddress, MHealthAddress, DeadAddress, TimerAddress, this);
+                voyeurThread = new Thread(bob.PleaseWatch);
+                voyeurThread.IsBackground = true;
+                voyeurThread.Start();
+                //ellie = new Goulding(this);
+                //lightsThread = new Thread(ellie.repetition);
+                //lightsThread.IsBackground = true;
+                //lightsThread.Start();
+                buttonBegin.Text = "Stop Evaluation";
+            }
+            else
+            {
+                //ellie.stopRepetition();
+                //lightsThread.Join();
+                bob.StopWatching();
+                voyeurThread.Join();
+                buttonBegin.Text = "Begin Evaluation";
+            }
+        }
+        #endregion
+        
+        #region Lights Functions
+        /*
+        Button[] lights;
+        public void turnOn(params int[] lightNumbers)
+        {
+            for (int i = 0; i < lightNumbers.Length; i++)
+            {
+                lights[i].BackColor = Color.Green;
+            }
+        }
+        public void turnOff(params int[] lightNumbers)
+        {
+            for (int i = 0; i < lightNumbers.Length; i++)
+            {
+                lights[i].BackColor = Color.Red;
+            }
+        }
+        public void onAll()
+        {
+            for (int i = 0; i < lights.Length; i++)
+            {
+                lights[i].BackColor = Color.Green;
+            }
+        }
+        public void offAll()
+        {
+            for (int i = 0; i < lights.Length; i++)
+            {
+                lights[i].BackColor = Color.Red;
+            }
+        }*/
+        #endregion
+    }
+
+    public class Watcher
+    {
+        Process Dota2;
+        IntPtr CHealthAddress;
+        IntPtr MHealthAddress;
+        IntPtr DeadAddress;
+        IntPtr TimerAddress;
+        Form1 Owner;
+
+        [DllImport("kernel32.dll")]
+        public static extern IntPtr OpenProcess(int dwDesiredAccess, bool bInheritHandle, int dwProcessId);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static extern bool ReadProcessMemory(
+            IntPtr hProcess,
+            IntPtr lpBaseAddress,
+            [Out] byte[] lpBuffer,
+            int dwSize,
+            out int lpNumberOfBytesRead
+        );
+
+        public Watcher(Process dota, IntPtr ch, IntPtr mh, IntPtr d, IntPtr t, Form1 _owner)
+        {
+            Dota2 = dota;
+            CHealthAddress = ch;
+            MHealthAddress = mh;
+            DeadAddress = d;
+            TimerAddress = t;
+            Owner = _owner;
+        }
+
+        private volatile bool working = true;
+
+        public void PleaseWatch()
+        {
+            IntPtr p = OpenProcess(0x10, true, Dota2.Id); //0x10-read
+            int bytesRead = 0;
+            while (working)
+            {
+                byte[] currentHealth = new byte[4];
+                ReadProcessMemory(p, CHealthAddress, currentHealth, 4, out bytesRead);
+                byte[] maxHealth = new byte[4];
+                ReadProcessMemory(p, MHealthAddress, maxHealth, 4, out bytesRead);
+                byte[] deadStatus = new byte[4];
+                ReadProcessMemory(p, DeadAddress, deadStatus, 4, out bytesRead);
+                byte[] respawnTimer = new byte[4];
+                ReadProcessMemory(p, TimerAddress, respawnTimer, 4, out bytesRead);
+                Owner.updateNumbers(BitConverter.ToInt32(currentHealth, 0), BitConverter.ToInt32(maxHealth, 0), BitConverter.ToInt32(deadStatus, 0), BitConverter.ToInt32(respawnTimer, 0));
+                Thread.Sleep(20);
+            }
+        }
+
+        public void StopWatching()
+        {
+            working = false;
+        }
+    }
+
+    public class ImpossibleExceptionException : Exception
+    {
+        public ImpossibleExceptionException() { }
+
+        public ImpossibleExceptionException(String message) : base(message) { }
+    }
+
+    /*
+    public class Goulding
+    {
+        private Form1 _parent;
+        private volatile bool working = true;
+
+        public Goulding(Form1 parent)
+        {
+            _parent = parent;
+        }
+
+        public void repetition()
+        {
+            int lightsOn = 0;
+            int startRespawnTime = 0;
+            while (working)
+            {
+                if (_parent.DeathStatus == 0)
+                {
+                    startRespawnTime = 0;
+                    double frac = _parent.CurrentHealth / _parent.MaximumHealth;
+                    int nowLights = (int)Math.Ceiling(frac * 10);
+                    if (nowLights > lightsOn)
+                    {
+                        int[] onCommand = new int[nowLights - lightsOn];
+                        for(int i=0;i<onCommand.Length;i++)
+                        {
+                            onCommand[i] = lightsOn + i;
+                        }
+                        _parent.turnOn(onCommand);
+                    }
+                    else if(lightsOn > nowLights)
+                    {
+                        int[] offCommand = new int[lightsOn - nowLights];
+                        for (int i = 0; i < offCommand.Length; i++)
+                        {
+                            offCommand[i] = nowLights - i;
+                        }
+                        _parent.turnOff(offCommand);
+                    }
+                }
+                else
+                {
+                    if (startRespawnTime == 0)
+                    {
+                        startRespawnTime = _parent.RespawnTimer;
+                        _parent.offAll();
+                    }
+                }
+            }
+        }
+        public void stopRepetition()
+        {
+            working = false;
+        }
+    }
+    */
+}
